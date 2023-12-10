b12e38a8f272f7b60e0745784695eb6a03e2cb5a
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-10 11:14:56 -0600

    Update sort.h

--- sort.h
+++ sort.h
@@ -20,7 +20,7 @@ void selection_sort(int *array, size_t size);
 
 void print_array(const int *array, size_t size);
 void quick_sort_helper(int *array, size_t low, size_t high);
-size_t lomuto_partition(int *array, size_t low, size_t high);
+int lomuto_partition(int *array, size_t low, size_t high) {
 void swap(int *a, int *b);
 
 
