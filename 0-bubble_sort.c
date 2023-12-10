2dad94c7c024980d1cfd8b992cb8b7de75fc5e18
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-10 01:41:53 -0600

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,6 +11,10 @@ void swap(int *a, int *b) {
 void bubble_sort(int *array, size_t size) {
     size_t i, j;
 
+    if (array == NULL || size < 2) {
+        return; 
+    }
+
     for (i = 0; i < size - 1; i++) {
         for (j = 0; j < size - i - 1; j++) {
             if (array[j] > array[j + 1]) {
