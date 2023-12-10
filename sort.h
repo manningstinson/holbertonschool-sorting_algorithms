fc9ce0440219c4c177e0f38922d14573b3cc6816
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-10 01:56:40 -0600

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,9 +1,16 @@
-
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * struct listint_s - Doubly linked list node structure
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
     const int n;
@@ -11,8 +18,11 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/* Provided print functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
+
+/* Sorting algorithms */
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
