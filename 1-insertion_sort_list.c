d0f65cfc486f0d72a02363845b0e9962b0d71deb
Manning Stinson <manning@manningstinson.com>
2023-12-10 13:23:38 -0800

    betty styles

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,38 +9,35 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current;
-    listint_t *temp;
+	listint_t *current;
+	listint_t *temp;
 
-    if (!list || !*list || !(*list)->next)
-        return;
+	if (!list || !*list || !(*list)->next)
+		return;
 
-    current = (*list)->next;
+	current = (*list)->next;
 
-    while (current)
-    {
-        temp = current->next;
+	while (current)
+	{
+		temp = current->next;
 
-        while (current->prev && current->n < current->prev->n)
-        {
-            listint_t *prev = current->prev;
-            listint_t *next = current->next;
+		while (current->prev && current->n < current->prev->n)
+		{
+			listint_t *prev = current->prev;
+			listint_t *next = current->next;
 
-            prev->next = next;
-            if (next)
-                next->prev = prev;
-            current->next = prev;
-            current->prev = prev->prev;
-            current->next->prev = current;
-
-            if (current->prev)
-                current->prev->next = current;
-            else
-                *list = current;
-
-            print_list(*list);
-        }
-
-        current = temp;
-    }
+			prev->next = next;
+			if (next)
+				next->prev = prev;
+			current->next = prev;
+			current->prev = prev->prev;
+			current->next->prev = current;
+			if (current->prev)
+				current->prev->next = current;
+			else
+				*list = current;
+			print_list(*list);
+		}
+		current = temp;
+	}
 }
