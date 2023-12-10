034baac9c2d93e7c1f989a663bbf68ad1a0bdb87
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-10 01:07:58 -0600

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,9 +1,11 @@
-// 0-bubble_sort.c
+/* 0-bubble_sort.c */
 #include <stdio.h>
 #include "sort.h"
 
 void bubble_sort(int *array, size_t size)
 {
+    (void)array; // Suppress unused parameter warning
+    (void)size;  // Suppress unused parameter warning
+
     // Implementation as provided earlier
 }
-
