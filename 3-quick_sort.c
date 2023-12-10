e247458b86807d4b459f25d4455eafe6b8d2f19f
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-10 10:53:28 -0600

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,10 +10,12 @@ void quick_sort(int *array, size_t size)
 
 void quick_sort_helper(int *array, size_t low, size_t high)
 {
+    size_t partition_index;
+
     if (low < high)
     {
-        size_t partition_index = lomuto_partition(array, low, high);
-        if (partition_index != 0) // Check if partition_index is not zero
+        partition_index = lomuto_partition(array, low, high);
+        if (partition_index != 0) /* Check if partition_index is not zero */
             quick_sort_helper(array, low, partition_index - 1);
         quick_sort_helper(array, partition_index + 1, high);
     }
@@ -23,10 +25,11 @@ size_t lomuto_partition(int *array, size_t low, size_t high)
 {
     int pivot = array[high];
     size_t i = low;
+    size_t j;
 
-    for (size_t j = low; j < high; ++j)
+    for (j = low; j < high; ++j)
     {
-        if (array[j] <= pivot) // Include equal to pivot elements
+        if (array[j] <= pivot) /* Include equal to pivot elements */
         {
             swap(&array[i], &array[j]);
             i++;
