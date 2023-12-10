12810f7c3758597e2053a4bcb15a1108b8fbed2c
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-10 11:54:42 -0600

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,36 +1,24 @@
 #include "sort.h"
+#include <stdint.h>
 
 void swap(int *a, int *b) {
-    int temp;
-    temp = *a;
+    int temp = *a;
     *a = *b;
     *b = temp;
 }
 
 size_t lomuto_partition(int *array, size_t low, size_t high) {
-    size_t i;
+    size_t i; // Moved declaration outside the loop
     size_t q = low - 1;
     int pivot = array[high];
-
     for (i = low; i < high; ++i) {
         if (array[i] <= pivot) {
             ++q;
             swap(&array[q], &array[i]);
         }
     }
-
     swap(&array[q + 1], &array[high]);
     return q + 1;
-}
-
-void quick_sort_helper(int *array, size_t low, size_t high) {
-    size_t pi;
-
-    if (low < high) {
-        pi = lomuto_partition(array, low, high);
-        quick_sort_helper(array, low, pi);       
-        quick_sort_helper(array, pi + 1, high);
-    }
 }
 
 void quick_sort(int *array, size_t size) {
@@ -38,4 +26,12 @@ void quick_sort(int *array, size_t size) {
         return;
     }
     quick_sort_helper(array, 0, size - 1);
+}
+
+void quick_sort_helper(int *array, size_t low, size_t high) {
+    if (low < high) {
+        size_t pi = lomuto_partition(array, low, high);
+        quick_sort_helper(array, low, pi - 1);
+        quick_sort_helper(array, pi + 1, high);
+    }
 }
