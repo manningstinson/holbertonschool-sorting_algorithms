8e9258eeb91ffa003e0e7d9f07d2d8ab1aeb3c44
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-10 13:49:25 -0600

    Update sort.h

--- sort.h
+++ sort.h
@@ -18,8 +18,4 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 
-void print_array(const int *array, size_t size);
-void quick_sort_helper(int *array, size_t low, size_t high);
-void swap(int *a, int *b);
-
 #endif /* SORT_H */
