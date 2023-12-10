ebd9dfc21c6e31ddc2c51536934e88743b14e9d4
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-10 15:02:01 -0600

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,11 @@
-
 #include <stdio.h>
 #include "sort.h"
 
+/**
+ * swap - Swaps two integers in an array
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
 void swap(int *a, int *b)
 {
 	int temp = *a;
@@ -9,14 +13,19 @@ void swap(int *a, int *b)
 	*b = temp;
 }
 
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using
+ *               the Bubble Sort algorithm
+ * @array: Array to be sorted
+ * @size: Number of elements in the array
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
 	if (array == NULL || size < 2)
-	{
 		return;
-	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
