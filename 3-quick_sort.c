fe8a97c2a85debfec559cdf492870201b0105728
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-10 13:51:24 -0600

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,47 +1,75 @@
 #include "sort.h"
 #include <stdio.h>
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void print_array(const int *array, size_t size) {
-    for (size_t i = 0; i < size; ++i) {
-        printf("%d", array[i]);
-        if (i < size - 1) {
-            printf(", ");
-        }
+/**
+ * swap - Swaps two integers in an array
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
+void swap(int *a, int *b)
+{
+    if (a != b)
+    {
+        *a ^= *b;
+        *b ^= *a;
+        *a ^= *b;
     }
-    printf("\n");
 }
 
-size_t lomuto_partition(int *array, size_t low, size_t high) {
-    size_t i;
-    size_t q = low - 1;
+/**
+ * lomuto_partition - Lomuto partition scheme for Quick Sort
+ * @array: Array to be partitioned
+ * @low: Starting index of the partition
+ * @high: Ending index of the partition
+ * @size: Size of the array
+ * Return: Index of the pivot element after partition
+ */
+size_t lomuto_partition(int *array, size_t low, size_t high, size_t size)
+{
     int pivot = array[high];
-    for (i = low; i < high; ++i) {
-        if (array[i] <= pivot) {
-            ++q;
-            swap(&array[q], &array[i]);
+    size_t i = low - 1, j;
+
+    for (j = low; j < high; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            i++;
+            swap(&array[i], &array[j]);
+            print_array(array, size);
         }
     }
-    swap(&array[q + 1], &array[high]);
-    return q + 1;
+
+    swap(&array[i + 1], &array[high]);
+    print_array(array, size);
+
+    return (i + 1);
 }
 
-void quick_sort(int *array, size_t size) {
-    if (size < 2) {
-        return;
+/**
+ * quicksort - Recursive function to perform Quick Sort
+ * @array: Array to be sorted
+ * @low: Starting index of the partition
+ * @high: Ending index of the partition
+ * @size: Size of the array
+ */
+void quicksort(int *array, size_t low, size_t high, size_t size)
+{
+    if (low < high)
+    {
+        size_t pivot_idx = lomuto_partition(array, low, high, size);
+
+        quicksort(array, low, pivot_idx - 1, size);
+        quicksort(array, pivot_idx + 1, high, size);
     }
-    quick_sort_helper(array, 0, size - 1);
 }
 
-void quick_sort_helper(int *array, size_t low, size_t high) {
-    if (low < high) {
-        size_t pi = lomuto_partition(array, low, high);
-        quick_sort_helper(array, low, pi);
-        quick_sort_helper(array, pi + 1, high);
-    }
+/**
+ * quick_sort - Wrapper function for Quick Sort
+ * @array: Array to be sorted
+ * @size: Size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+    if (array && size > 1)
+        quicksort(array, 0, size - 1, size);
 }
