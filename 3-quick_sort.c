c004fd15697bd057e5120868b0021a26352a3a0c
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-10 14:30:26 -0600

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,19 +7,18 @@
  * @j: variable int
  * @size: variable size
  */
-
-void _swap(int *array, int i, int j, size_t size)
+void _swap(int *array, int i, int j)
 {
-	int tmp;
-
-	if (array[i] != array[j])
-	{
-		tmp = array[i];
-		array[i] = array[j];
-		array[j] = tmp;
-		print_array(array, size);
-	}
+    /* Check if values are different to avoid unnecessary operations */
+    if (array[i] != array[j])
+    {
+        /* XOR operation to swap element values */
+        array[i] ^= array[j];
+        array[j] ^= array[i];
+        array[i] ^= array[j];
+    }
 }
+
 
 /**
  * partition - function sort a partition
