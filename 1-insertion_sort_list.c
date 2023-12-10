6e92a404763baafd4a9a1e37f0c41076ae59c7cd
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-10 02:50:59 -0600

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,18 +1,12 @@
-#include "sort.h"
-#include <stdio.h>
-
-/**
- * insertion_sort_list - Sorts a doubly linked list in ascending order
- *
- * @list: Pointer to the head of the list
- */
 void insertion_sort_list(listint_t **list)
 {
+    listint_t *current;
+    listint_t *temp;
+
     if (!list || !*list || !(*list)->next)
         return;
 
-    listint_t *current = (*list)->next;
-    listint_t *temp;
+    current = (*list)->next;
 
     while (current)
     {
@@ -20,11 +14,14 @@ void insertion_sort_list(listint_t **list)
 
         while (current->prev && current->n < current->prev->n)
         {
-            current->prev->next = current->next;
-            if (current->next)
-                current->next->prev = current->prev;
-            current->next = current->prev;
-            current->prev = current->prev->prev;
+            listint_t *prev = current->prev;
+            listint_t *next = current->next;
+
+            prev->next = next;
+            if (next)
+                next->prev = prev;
+            current->next = prev;
+            current->prev = prev->prev;
             current->next->prev = current;
 
             if (current->prev)
