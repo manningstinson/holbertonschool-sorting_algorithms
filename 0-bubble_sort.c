48f7d5784021cfbb299558f1660300f43c7e17f6
Manning Stinson <manning@manningstinson.com>
2023-12-10 12:52:41 -0800

    betty styles

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,31 @@
-/* 0-bubble_sort.c */
+
 #include <stdio.h>
 #include "sort.h"
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
-void bubble_sort(int *array, size_t size) {
-    size_t i, j;
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
 
-    if (array == NULL || size < 2) {
-        return; 
-    }
-
-    for (i = 0; i < size - 1; i++) {
-        for (j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                swap(&array[j], &array[j + 1]);
-                print_array(array, size);
-            }
-        }
-    }
+	if (array == NULL || size < 2)
+	{
+		return (0);
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
 }
