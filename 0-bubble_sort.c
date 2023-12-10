1ef9f108e8245cbeedbcf170279ee80d53e59a53
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-10 01:32:33 -0600

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,4 @@
-/* 0-bubble_sort.c */
+
 #include <stdio.h>
 #include "sort.h"
 
@@ -9,14 +9,19 @@ void swap(int *a, int *b) {
 }
 
 void bubble_sort(int *array, size_t size) {
+    if (array == NULL || size < 2) {
+        return;  // No need to sort if the array is NULL or has less than 2 elements
+    }
+
     size_t i, j;
 
     for (i = 0; i < size - 1; i++) {
         for (j = 0; j < size - i - 1; j++) {
             if (array[j] > array[j + 1]) {
                 swap(&array[j], &array[j + 1]);
-                print_array(array, size);
             }
         }
+        print_array(array, size);
     }
 }
+
