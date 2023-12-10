34b6227cc0dbdb5ff298cac82dae5606ccb57630
Manning Stinson <manning@manningstinson.com>
2023-12-10 07:28:48 -0800

    updated sort.h

--- sort.h
+++ sort.h
@@ -11,16 +11,11 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-void print_array(const int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void write_char(char c);
-void write_num(int num);
-void quick_sort_helper(int *array, size_t low, size_t high);
-size_t lomuto_partition(int *array, size_t low, size_t high);
-void swap(int *a, int *b);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+
 
 #endif /* SORT_H */
