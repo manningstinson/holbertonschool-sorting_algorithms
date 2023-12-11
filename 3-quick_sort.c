2b58aa9c95ee64b2b0e5c13540d8c27a047f194d
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-10 20:05:06 -0600

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,81 +1,38 @@
-#include "sort.h"
+#include <stdlib.h>
 
-/**
- * _swap - change two positions
- * @array: variable int
- * @i: variable int
- * @j: variable int
- * @size: variable size
- */
-
-void _swap(int *array, int i, int j, size_t size)
-{
-	int tmp;
-
-	if (array[i] != array[j])
-	{
-		tmp = array[i];
-		array[i] = array[j];
-		array[j] = tmp;
-		print_array(array, size);
-	}
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-/**
- * partition - function sort a partition
- * @array: variable int
- * @start: variable int
- * @end: variable int
- * @size: variable size
- * Return: int
- */
+int partition(int *array, size_t low, size_t high) {
+    int pivot = array[high];
+    size_t i = low - 1;
 
-int partition(int *array, int start, int end, size_t size)
-{
-	int i = start, j, pivot = array[end];
-
-	for (j = start; j <= end; j++)
-	{
-		if (array[j] < pivot)
-		{
-			_swap(array, i, j, size);
-			i++;
-		}
-	}
-	_swap(array, i, end, size);
-	return (i);
+    for (size_t j = low; j <= high; j++) {
+        if (array[j] <= pivot) {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    return i;
 }
 
-/**
- * quicksort - function sorts an array of integers in ascending
- * @array: variable int
- * @start: variable int
- * @end: variable int
- * @size: variable size
- */
-
-void quicksort(int *array, int start, int end, size_t size)
-{
-	int pivot;
-
-	if (start >= end)
-	{
-		return;
-	}
-	pivot = partition(array, start, end, size);
-	quicksort(array, start, pivot - 1, size);
-	quicksort(array, pivot + 1, end, size);
+void quick_sort(int *array, size_t low, size_t high) {
+    if (low < high) {
+        size_t pi = partition(array, low, high);
+        quick_sort(array, low, pi - 1);
+        quick_sort(array, pi + 1, high);
+    }
 }
 
-/**
- * quick_sort - function sorts an array of integers in ascending
- * @array: variable int
- * @size: variable size
- */
-
-void quick_sort(int *array, size_t size)
-{
-	if (size < 2)
-		return;
-	quicksort(array, 0, size - 1, size);
+void print_array(int *array, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        printf("%d", array[i]);
+        if (i < size - 1) {
+            printf(", ");
+        }
+    }
+    printf("\n");
 }
